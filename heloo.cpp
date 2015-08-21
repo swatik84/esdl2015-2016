5bca520530f97bb924ab8694f2b7101447e417e3
swatik84 <shirsath.swati@gmail.com>
2015-08-21 08:38:50 +0530

    Update heloo.cpp

--- heloo.cpp
+++ heloo.cpp
@@ -1,6 +1,10 @@
 #includes<iostram>
 void main(){
-cout<<"hi....";
+cout<<"hello";
 cout<<"this is first file in mastr branch";
+cout<<"this is 1st uodate in file";
 cout<<"exit...";
+a=b*c;
+c=a*b;
+d=5*6;
 }
